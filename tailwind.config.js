1c94d312681850112e266376737c70968cb72ac4
fabzy100 <168279701+fabzy100@users.noreply.github.com>
2024-05-21 13:37:00 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  
+  color:{
+    'redText': '#Df6951',
+    'purpleText': '#181E4B',
+    'lightPurpleTest': '#5E6282',
+    'yellowcolor': '#F1A501',
+    'textWhite': '#fff',
+    'buttonColor': '#Df6951',
+  }
+}
